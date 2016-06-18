1a3048167ad313fa7d9d300f1ce2f49c1d878bed
Amir Fahd Hadji Usop <dudeamirfahd@gmail.com>
2016-06-18 20:36:24 +0800

    Add scene to scene navigation

---
+++ UnityUI/Assets/LoginRegister/Scripts/LoginFunctions.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoginFunctions : MonoBehaviour {
+
+    private InputField usernameInput;
+    private InputField passwordInput;
+    private Button loginButton;
+    private Button registerButton;
+
+	// Use this for initialization
+	void Start () {
+        usernameInput = GetComponent<InputField>();
+        passwordInput = GetComponent<InputField>();
+        loginButton = GetComponent<Button>();
+        registerButton = GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoginClicked()
+    {
+        Debug.Log("Login");
+    }
+
+    public void RegisterClicked()
+    {
+        Debug.Log("Register");
+        SceneManager.LoadScene("RegisterScene");
+    }
+}
---
+++ UnityUI/Assets/LoginRegister/Scripts/RegisterFunctions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class RegisterFunctions : MonoBehaviour {
+
+    private InputField emailInput;
+    private InputField usernameInput;
+    private InputField passwordInput;
+    private InputField confirmPasswordInput;
+    private InputField fullnameInput;
+    private ToggleGroup genderToggleGroup;
+    private Button submitButton;
+    private Button cancelButton;
+
+    // Use this for initialization
+    void Start () {
+        emailInput = GetComponent<InputField>();
+        usernameInput = GetComponent<InputField>();
+        passwordInput = GetComponent<InputField>();
+        confirmPasswordInput = GetComponent<InputField>();
+        fullnameInput = GetComponent<InputField>();
+        genderToggleGroup = GetComponent<ToggleGroup>();
+        submitButton = GetComponent<Button>();
+        cancelButton = GetComponent<Button>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void SubmitClicked()
+    {
+        Debug.Log("Submit");
+    }
+
+    public void CancelClicked()
+    {
+        Debug.Log("Cancel");
+        SceneManager.LoadScene("LoginScene");
+    }
+}
